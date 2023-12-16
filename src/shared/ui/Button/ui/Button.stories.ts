4ed67f4e0f04b4490e99be1ff404c5f16463fe6d
Ilya R <rogachev.ilya@list.ru>
2023-12-16 19:17:34 +0300

    eslint fix

--- src/shared/ui/Button/ui/Button.stories.ts
+++ src/shared/ui/Button/ui/Button.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import {Button, ButtonTheme} from "./Button";
+import {Button, ButtonTheme} from './Button';
 
 const meta = {
   title: 'Shared/Button',
