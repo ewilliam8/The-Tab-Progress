ece7bc3433fc284a4c6e11360572aa3965c8ef85
Ilya R <rogachev.ilya@list.ru>
2023-12-16 19:06:22 +0300

    storybook added

---
+++ src/shared/ui/Button/ui/Button.stories.ts
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {Button, ButtonTheme} from "./Button";
+
+const meta = {
+  title: 'Shared/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: 'Button',
+  },
+};
+
+export const Outlined: Story = {
+  args: {
+    theme: ButtonTheme.OUTLINED,
+    children: 'Button',
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    theme: ButtonTheme.ROUNDED,
+    children: 'Button',
+  },
+};
