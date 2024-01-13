5a838c38b6bf92f643f2daf725df506bd2ea5701
Ilya R <rogachev.ilya@list.ru>
2024-01-13 23:45:09 +0300

    fix(navbar) link to dashboard

--- src/features/Navbar/ui/Navbar.tsx
+++ src/features/Navbar/ui/Navbar.tsx
@@ -1,7 +1,9 @@
 import styles from './Navbar.module.scss'
 import { useLogout } from '@/entities/Session/api/useLogout'
 import { Button } from '@/shared/ui/Button'
-import { APP_NAME } from '@/shared/consts/app';
+import { APP_NAME } from '@/shared/consts/app'
+import Link from 'next/link'
+import { Routes } from '@/shared/config/routes'
 
 export const Navbar = () => {
     const { onLogOut } = useLogout()
@@ -9,7 +11,10 @@ export const Navbar = () => {
     return (
         <header className={styles.Navbar}>
             <div className={styles.items}>
-                {APP_NAME}
+                <Link href={Routes.DASHBOARD} >
+                    {APP_NAME}
+                </Link>
+
                 <Button
                     onClick={onLogOut}
                     variant={'secondary'}
