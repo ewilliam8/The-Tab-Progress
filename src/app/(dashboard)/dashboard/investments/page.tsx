3108500725a60c170666328bb6c5b229ad97c4d4
Ilya R <rogachev.ilya@list.ru>
2024-01-14 22:46:23 +0300

    refactor(header) renamed Navbar to Header

--- src/app/(dashboard)/dashboard/investments/page.tsx
+++ src/app/(dashboard)/dashboard/investments/page.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import { Navbar } from '@/features/Navbar'
-
 export default function InvestmentPage() {
     return (
         <div>
-            <Navbar />
             Investments Page
         </div>
     )
--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '@/app/(dashboard)/providers/StoreProvider'
 import { AuthProvider } from '@/app/(dashboard)/providers/AuthProvider'
-import { Header } from '@/features/Navbar'
+import { Header } from '@/features/Header'
 
 type RootLayoutProps = {
     children: ReactNode
