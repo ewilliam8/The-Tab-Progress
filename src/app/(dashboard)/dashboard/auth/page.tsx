97b1de06265328a9c51069221f4321c86079e7cf
Ilya R <rogachev.ilya@list.ru>
2024-01-20 11:03:53 +0300

    refactor(metadata): added metadata to pages

--- src/app/(dashboard)/dashboard/auth/page.tsx
+++ src/app/(dashboard)/dashboard/auth/page.tsx
@@ -1,5 +1,11 @@
 import styles from './AuthPage.module.scss'
 import { AuthForm } from '@/features/AuthForm'
+import { APP_DESC, APP_NAME } from '@/shared/consts/app';
+
+export const metadata = {
+    title: 'Login: ' + APP_NAME,
+    description: APP_DESC,
+}
 
 export default function Auth() {
     return (
--- src/app/(dashboard)/dashboard/investments/page.tsx
+++ src/app/(dashboard)/dashboard/investments/page.tsx
@@ -1,4 +1,9 @@
-'use client'
+import { APP_DESC, APP_NAME } from '@/shared/consts/app';
+
+export const metadata = {
+    title: 'Investments: ' + APP_NAME,
+    description: APP_DESC,
+}
 
 export default function InvestmentPage() {
     return (
