0489faa8911ff58e8a61c65a2c880263afd555fd
Ilya R <rogachev.ilya@list.ru>
2024-02-24 16:56:03 +0300

    feat(Custom404): added img to Not Found Page

--- src/app/(public)/[...not_found]/page.tsx
+++ src/app/(public)/[...not_found]/page.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export default function Custom404() {
-    return <h2>404 <br /> Page Not Found</h2>
+    return <div className={'mx-auto flex flex-col gap-8 items-center pt-24'}>
+        <Image
+            priority
+            src='/images/not_found.svg'
+            alt="Not Found"
+            width={400}
+            height={400}
+        />
+        <h2>Page Not Found</h2>
+    </div>
 }
